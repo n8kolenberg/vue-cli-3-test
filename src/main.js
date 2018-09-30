99f0ac96e4ece61ce8683fa1dc59191fc68e7145
Nathaniel Kolenberg <nkolenberg@gmail.com>
2018-09-30 11:27:31 +0200

    Add env environments and gitignore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'font-awesome/css/font-awesome.css'
 
 Vue.config.productionTip = false
 
